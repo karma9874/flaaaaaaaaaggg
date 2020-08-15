ffb168279761dec61fd0c4857f534447108afaf6
karma9874 <33444666+karma9874@users.noreply.github.com>
2020-08-15 12:33:14 +0530

    first commit

---
+++ flag.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define HN {
+#define IB }
+#define Bl8 int
+#define i1 (
+#define i0 )
+#define g6 [
+#define g7 ]
+#define T4 =
+#define T9 +
+#define GA4 while
+#define GA9 <
+#define JG6 char
+#define bye return
+#define l6 ,
+#define DU printf
+#define BP ;
+Bl8 main i1 i0 HN
+    Bl8 a g6 31 g7 T4 HN 100 l6 97 l6 114 l6 107 l6 67 l6 84 l6 70 l6 123 l6 110 l6 48 l6 112 l6 51 l6 95 l6 55 l6 104 l6 49 l6 53 l6 95 l6 110 l6 48 l6 55 l6 95 l6 55 l6 104 l6 51 l6 95 l6 102 l6 49 l6 52 l6 57 l6 125 IB BP Bl8 i T4 0 BP Bl8 j T4 1 BP GA4 i1 i GA9 31 i0 HN DU i1 "%c" l6 i1 JG6 i0 a g6 i g7 i0 BP i++ BP IB bye 0 BP IB
